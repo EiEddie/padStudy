fdf464a375e1a1e13a355edcf89d227d3f211a2d
EiEddie <55616830+EiEddie@users.noreply.github.com>
2021-02-19 23:32:16 +0800

    Update padStudyUserHome.cpp

--- padStudyUserHomeCpp/padStudyUserHome.cpp
+++ padStudyUserHomeCpp/padStudyUserHome.cpp
@@ -4,7 +4,7 @@
 #include<sqlite3.h>
 using namespace std;*/
 #include"variable.cpp"
-#include"../md5/md5.cpp"
+#include"..\md5\md5.cpp"
 #include<sqlite3.h>
 
 static int callback(void*, int, char**, char**);
@@ -13,7 +13,7 @@ void StudyUserHomeIn(string usFilePath,
 		string usClassName) {
 	sqlite3 *db;
 	char* zErrMsg = 0;
-	int temp = sqlite3_open("../userdata.db", &db);
+	int temp = sqlite3_open("..\\userdata.db", &db);
 	if(temp) {
 		cout << " Open database unsuccessfully: " 
 			<< sqlite3_errmsg(db) << endl;
@@ -49,13 +49,14 @@ void StudyUserHomeIn(string usFilePath,
 	sqlite3_close(db);
 
 	ifstream in(usFilePath.c_str(), ios::binary);
-	string a = "../UserHomeFile/";
+	string a = "..\\UserHomeFile\\";
 	ofstream out((a+usFileName).c_str(), ios::binary);
 	if(!in || !out) {
 		cout << " Copy file unsuccessfully\n";
 		return;
 	} else {
 		cout << " Copy file successfully\n";
+	}
 	out << in.rdbuf();
 	in.close();
 	out.close();
